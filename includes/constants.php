d27b786f0918f5b1a9753cde231d95ed84fc5933
Rohit Batra <rohit@rohitbatra.in>
2018-07-28 16:44:15 +0530

    Default Constants file

---
+++ includes/constants.php
@@ -0,0 +1,36 @@
+<?php
+
+
+/**
+ * Class Db2Config
+ * @note Constant with IBM DB2 connection parameters
+ */
+class Db2Config
+{
+    const DB_HOST = "xx.xx.xx.xx";
+    const DB_USER = "sample_user";
+    const DB_PASS = "sample_pass";
+    const DB_NAME = "sample_db";
+    const DB_DRIVER = "sample_db";
+    const DB_PORT = "50000";
+
+}
+
+
+/**
+ * Class DbConfig
+ * @note Constants with MySQL connection parameters
+ */
+class DbConfig
+{
+    const DB_HOST = "xx.xx.xx.xx";
+    const DB_USER = "sample_user";
+    const DB_PASS = "sample_pass";
+    const DB_NAME = "sample_db";
+}
+
+class AppParms
+{
+    const SERVER_TIMEZONE = "America/Chicago";
+    const BASE_DIR = "";
+}
